810a5294a576dc8027fed9dea90b15dfaceac911
Cody Dupuis <codydupuis99@gmail.com>
2020-04-15 00:45:57 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,6 @@ def introduction(name)
 puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language(english)(spanish)
-puts "Hi, my name is #{english}#{spanish}"
+def introduction_with_language(name)
+puts "Hi, my name is #{name)"
 end
